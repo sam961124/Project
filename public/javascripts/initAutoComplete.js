940b28aed1df6ff302d17303458aa88c14932752
lkh9543 <lkh9543@gmail.com>
2016-11-05 00:36:14 +0800

    ssl v1

--- public/javascripts/initAutoComplete.js
+++ public/javascripts/initAutoComplete.js
@@ -13,33 +13,19 @@ function initMap() {
         mapTypeControl: false
     }
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    var mapSet = false;
     //check whether browser supports GPS locating
-    var setDefaultPos = function() {
-        mapSet = true;
-        myCenter.lat = 25.021719;
-        myCenter.lng = 121.534958;
-        var myLatlng = new google.maps.LatLng(myCenter.lat, myCenter.lng);
-        addMarker(myLatlng);
-    }
-    /*if (navigator.geolocation) {
-        try {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                addMarker(location);
-            })
-        } catch (e) {
-            setDefaultPos();
-        }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            addMarker(location);
+        });
     } else {
-        setDefaultPos();
-    };*/
-
-    if (!mapSet)
-        setDefaultPos();
-
+        location = new google.maps.LatLng(25.09108, 121.5598);
+        addMarker(location);
+    }
     showHospital();
 }
+
 //add User location marker
 function addMarker(location) {
     var marker = new google.maps.Marker({
